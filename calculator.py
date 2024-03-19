96fd3c31389d7914a730eccc084521459c221598
greenyun2 <cys4875@naver.com>
2024-03-19 22:49:07 +0900

    Commit Change

--- calculator.py
+++ calculator.py
@@ -8,13 +8,8 @@ def subtract(a, b):
 def multiply(a, b):
   return a*b
 
-<<<<<<< HEAD
-def divide_free(a, b):
-    return a/b
+def divide_new(a, b):
+  return a/b
 
 def getRemainder(a, b):
     return a//b
-=======
-def divide_new(a, b):
-  return a/b
->>>>>>> 95c17b5640888114139f78fb55a3d35f58fa6f0c
